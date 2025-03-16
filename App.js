b51cd23bab73d73d6d8794c5c6f36aa2323260a8
chris <chrisbriollovehannah@gmail.com>
2025-03-16 16:10:32 +0800

    app.js changed

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import search from './Screens/searchPage'
 SplashScreen.preventAutoHideAsync();
 
 const Stack = createStackNavigator();
-const e = "Hannah loves me"
+const e = "jaee loves me"
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
